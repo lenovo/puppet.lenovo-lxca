97b08db2bf8c8086f104037f2b09e2acc682679e
Naval Patel <npatel5@lenovo.com>
2018-07-10 07:18:14 -0400

    added config tests

---
+++ spec/unit/type/lxca_config_pattern_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+describe 'lxca_config_pattern', type: :type do
+  let(:type_class) { Puppet::Type.type(:lxca_config_pattern) }
+
+  let :params do
+    [
+      :name,
+      :id,
+      :endpoints,
+      :restart,
+      :etype,
+      :import_json,
+    ]
+  end
+
+  let :properties do
+    [
+    ]
+  end
+
+  it 'does have expected properties' do
+    properties.each do |property|
+      expect(type_class.properties.map(&:name)).to be_include(property)
+    end
+  end
+
+  it 'does have expected parameters' do
+    params.each do |param|
+      expect(type_class.parameters).to be_include(param)
+    end
+  end
+
+  it 'does require a name' do
+    expect { type_class.new({}) }.to raise_error(Puppet::Error, 'Title or name must be provided')
+  end
+
+  it 'does support :present as a value to :ensure' do
+    type_class.new(name: 'config pattern ', id: '12', ensure: :present)
+  end
+
+  it 'does support :filter_by_id as a value to :ensure' do
+    type_class.new(name: 'config pattern by id ', id: '12', ensure: :filter_by_id)
+  end
+
+  it 'does support :export_config_pattern as a value to :ensure' do
+    type_class.new(name: 'export config pattern', id: '12', ensure: :export_config_pattern)
+  end
+
+  it 'does support :deploy_config_pattern as a value to :ensure' do
+    type_class.new(name: 'deploy config pattern', id: '12',
+                      endpoints: 'B918EDCA1B5F11E2803EBECB82710ADE',
+                      restart: 'pending',
+                      etype: 'node',
+                      ensure: :deploy_config_pattern)
+
+  end
+
+  it 'does support :import_config_pattern as a value to :ensure' do
+    type_class.new(name: 'import config pattern',
+                      import_json:  '{"template_type" : "SystemInfo","template" : {"contact" : "contact","description" : "Pattern created by ruby test API ","location" : "location","name" : "Learned-System_Info-99","systemName" : {"autogen" : "Disable","hyphenChecked" : "FALSE"},"type" : "SystemInfo","uri" : "\/config\/template\/62","userDefined" : "TRUE"}}',
+                      ensure: :import_config_pattern)
+  end
+end
---
+++ spec/unit/type/lxca_config_profile_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe 'lxca_config_profile', type: :type do
+  let(:type_class) { Puppet::Type.type(:lxca_config_profile) }
+
+  let :params do
+    [
+      :name,
+      :id,
+      :profile_name,
+      :endpoint_uuid,
+      :restart,
+      :power_down,
+      :reset_imm,
+      :force,
+    ]
+  end
+
+  let :properties do
+    [
+    ]
+  end
+
+  it 'does have expected properties' do
+    properties.each do |property|
+      expect(type_class.properties.map(&:name)).to be_include(property)
+    end
+  end
+
+  it 'does have expected parameters' do
+    params.each do |param|
+      expect(type_class.parameters).to be_include(param)
+    end
+  end
+
+  it 'does require a name' do
+    expect { type_class.new({}) }.to raise_error(Puppet::Error, 'Title or name must be provided')
+  end
+
+  it 'does support :present as a value to :ensure' do
+    type_class.new(name: 'config profile ', id: '12', ensure: :present)
+  end
+
+  it 'does support :discover_all as a value to :ensure' do
+    type_class.new(name: 'all config profile ', ensure: :discover_all)
+  end
+
+  it 'does support :filter_by_id as a value to :ensure' do
+    type_class.new(name: 'config profile by id ', id: '12', ensure: :filter_by_id)
+  end
+
+  it 'does support :unassign_config_profile as a value to :ensure' do
+    type_class.new(name: 'unassign config profile ', id: '12',
+                     power_down: 'False',
+                     reset_imm: 'False',
+                     force: 'False',
+                     ensure: :unassign_config_profile)
+  end
+
+  it 'does support :delete_config_profile as a value to :ensure' do
+    type_class.new(name: 'delete config profile by id ', id: '12', ensure: :delete_config_profile)
+  end
+
+  it 'does support :rename_config_profile as a value to :ensure' do
+    type_class.new(name: 'rename existing config profile', id: '12', profile_name: 'new_profile_name', ensure: :rename_config_profile)
+  end
+
+  it 'does support :activate_config_profilen as a value to :ensure' do
+    type_class.new(name: 'activate config profile', id: '12',
+                      endpoint_uuid: '46920C143355486F97C19A34ABC7D746_bay10',
+                      restart: 'defer',
+                      ensure: :activate_config_profile)
+  end
+end
